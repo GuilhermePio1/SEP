8ee527ed66b91b9f480cfc57a02f34c624fa8b4f
Severino Carvalho <severinocarvalho14@gmail.com>
2024-11-29 01:12:05 -0300

    SEP-2: Adiciona configurações do docker-compose e SQL com a criação de um usuário padrão

---
+++ src/main/resources/db/sep-seed-database.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuarios (nome, email, senha)
+VALUES (
+        'Administrador do Sistema',
+        'admin@sep.com.br',
+        '$2a$10$1Otln/Qp6KdtHyBMRaH/Fe7wpy8B9ai4b/k0B5tgHlJsPmYBXRxYS'
+);
